6f9c9d2e2b8cb44a12cc92603bc766d51c301fb5
Svetlana Yakutina <132339674+SvetlanaYakutinaa@users.noreply.github.com>
2024-06-05 11:59:54 +0200

    Update Gottesacker_Herrnhut.py

--- Gottesacker_Herrnhut.py
+++ Gottesacker_Herrnhut.py
@@ -1,25 +1,32 @@
 import streamlit as st
+from streamlit_folium import st_folium
 import folium
 from streamlit.components.v1 import html
 
-# Setze den Titel der Streamlit-Anwendung
-st.title("Interaktive Karte mit Streamlit und Folium")
+st.set_page_config(page_title="Gottesacker Herrnhut", layout="wide")
 
-# Erstelle eine Basis-Karte
-map_center = [48.8566, 2.3522]  # Koordinaten für Paris
-m = folium.Map(location=map_center, zoom_start=12)
+# Seitenleiste mit Links zu verschiedenen Seiten erstellen
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Gehe zu", ["Karte", "Analyse"])
 
-# Füge Marker zur Karte hinzu
-folium.Marker([48.8566, 2.3522], popup="Paris", tooltip="Klicken für mehr Info").add_to(m)
-folium.Marker([48.8584, 2.2945], popup="Eiffelturm", tooltip="Klicken für mehr Info").add_to(m)
-folium.Marker([48.853, 2.3499], popup="Notre-Dame", tooltip="Klicken für mehr Info").add_to(m)
+# Seiteninhalt basierend auf der Auswahl des Benutzers aktualisieren
+if selection == "Karte":
 
-# Speichere die Karte in einer HTML-Datei
-m.save("map.html")
+     # Die Karte auf der Webseite posten
+     with st.container():
+          st.title("Karte des Gottesackers in Herrnhut")
+          
+          # Koordinaten für die Anfangsanzeige der Karte (zum Beispiel: Berlin)
+          start_coordinates = (51.019419, 14.748778)
+          m = folium.Map(location=start_coordinates, zoom_start=12)
 
-# Lese die HTML-Datei und zeige sie in Streamlit an
-with open("map.html", "r", encoding="utf-8") as f:
-    map_html = f.read()
+          # Speichere die Karte in einer HTML-Datei
+          m.save("map.html")
 
-# Zeige die HTML-Karte in der Streamlit-Anwendung
-html(map_html, height=500)
+          # Lese die HTML-Datei und zeige sie in Streamlit an
+          with open("map.html", "r", encoding="utf-8") as f:
+              map_html = f.read()
+
+          # Zeige die HTML-Karte in der Streamlit-Anwendung
+          html(map_html, height=500)
+
