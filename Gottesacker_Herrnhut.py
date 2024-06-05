9c802ecfe858098e83e246291c4a056c60e19dc3
Svetlana Yakutina <132339674+SvetlanaYakutinaa@users.noreply.github.com>
2024-06-05 11:57:45 +0200

    Update Gottesacker_Herrnhut.py

--- Gottesacker_Herrnhut.py
+++ Gottesacker_Herrnhut.py
@@ -2,15 +2,20 @@ import streamlit as st
 import folium
 from streamlit.components.v1 import html
 
-st.title("Karte des Gottesackers Herrnhut")
+# Setze den Titel der Streamlit-Anwendung
+st.title("Interaktive Karte mit Streamlit und Folium")
 
-map_center = [51.019419, 14.748778]
-m = folium.Map(location=map_center, zoom_start=19)
+# Erstelle eine Basis-Karte
+map_center = [48.8566, 2.3522]  # Koordinaten für Paris
+m = folium.Map(location=map_center, zoom_start=12)
 
+# Füge Marker zur Karte hinzu
+folium.Marker([48.8566, 2.3522], popup="Paris", tooltip="Klicken für mehr Info").add_to(m)
+folium.Marker([48.8584, 2.2945], popup="Eiffelturm", tooltip="Klicken für mehr Info").add_to(m)
+folium.Marker([48.853, 2.3499], popup="Notre-Dame", tooltip="Klicken für mehr Info").add_to(m)
 
-folium.Marker([51.05853, 14.4437662], popup="Andersen").add_to(m)
-
-m.save("mape.html")
+# Speichere die Karte in einer HTML-Datei
+m.save("map.html")
 
 # Lese die HTML-Datei und zeige sie in Streamlit an
 with open("map.html", "r", encoding="utf-8") as f:
@@ -18,4 +23,3 @@ with open("map.html", "r", encoding="utf-8") as f:
 
 # Zeige die HTML-Karte in der Streamlit-Anwendung
 html(map_html, height=500)
-
