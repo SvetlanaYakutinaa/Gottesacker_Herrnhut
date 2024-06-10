e7933ca88f97fa71bd7f83091651c36ae0157494
Svetlana Yakutina <132339674+SvetlanaYakutinaa@users.noreply.github.com>
2024-06-10 13:41:27 +0200

    Update Gottesacker_Herrnhut.py

--- Gottesacker_Herrnhut.py
+++ Gottesacker_Herrnhut.py
@@ -3,7 +3,6 @@ import folium
 from streamlit.components.v1 import html
 import json
 from folium import IFrame
-from streamlit_folium import folium_static
 
 st.set_page_config(page_title="Gottesacker Herrnhut", layout="wide")
 
@@ -22,32 +21,19 @@ if selection == "Karte":
         start_coordinates = (51.019419, 14.748778)
         m = folium.Map(location=start_coordinates, zoom_start=18)
 
-        folium.Marker([51.019529, 14.748889], popup="Knud Andersen").add_to(m)    
+        folium.Marker([51.019529, 14.748889], popup="Knud Andersen").add_to(m)
         
-        # Bild-URL
-        img_url = 'https://example.com/path/to/your/image.jpg'  # Ersetze durch die tatsächliche URL deines Bildes
-        
-        # HTML-Code für das Bild
-        html = f'<img src="{img_url}" width="300" height="200">'
-        
-        # Erstelle ein IFrame-Objekt
-        iframe = folium.IFrame(html, width=310, height=210)
-        
-        # Erstelle ein Popup mit dem IFrame
-        popup = folium.Popup(iframe, max_width=310)
-        
-        # Erstelle den Marker mit dem Popup
-        folium.Marker([51.019529, 14.748889], popup=popup).add_to(m)
-        
-        # Rendere die Karte in Streamlit
-        folium_static(m)
+        # Speichere die Karte in einer HTML-Datei
+        m.save("map.html")
 
         # Lese die HTML-Datei und zeige sie in Streamlit an
         with open("map.html", "r", encoding="utf-8") as f:
             map_html = f.read()
 
         # Zeige die HTML-Karte in der Streamlit-Anwendung
-        st.components.v1.html(map_html, height=500)
+        st.components.v1.html(map_html, height=500)    
+        
+        
         
 
 
