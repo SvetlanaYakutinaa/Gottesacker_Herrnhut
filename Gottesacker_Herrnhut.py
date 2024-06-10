670c3b7708649506577c2acdda57e7e44aaf1ae0
Svetlana Yakutina <132339674+SvetlanaYakutinaa@users.noreply.github.com>
2024-06-10 15:23:17 +0200

    Update Gottesacker_Herrnhut.py

--- Gottesacker_Herrnhut.py
+++ Gottesacker_Herrnhut.py
@@ -18,15 +18,10 @@ if selection == "Karte":
     # Die Karte auf der Webseite posten
     with st.container():
         st.title("Karte des Gottesackers Herrnhut")
-        
-        # Koordinaten für die Anfangsanzeige der Karte (zum Beispiel: Berlin)
-        start_coordinates = (51.019419, 14.748778)
-        m = folium.Map(location=start_coordinates, zoom_start=18)
-        
-        # HTML-Code für das Popup mit der JPEG-Datei
+
+        # HTML-Code für das Popup ohne Bild
         html = """
-        <h1>Ein Bild in einem Popup</h1><br>
-        <img src="https://github.com/SvetlanaYakutinaa/Gottesacker_Herrnhut/blob/main/Gottesacker_Julius_Titz/Andersen.jpeg" alt="Dein Bild" width="400" height="300">
+        <h1>Popup ohne Bild</h1>
         <p>
         Hier ist etwas Beispieltext.
         </p>
@@ -39,10 +34,9 @@ if selection == "Karte":
         popup = folium.Popup(iframe, max_width=500)
         
         # Marker mit Popup zur Karte hinzufügen 
-        #Marker mit Popup zur Karte hinzufügen
         marker_coordinates = [51.019529, 14.748889]  # Koordinaten des Markers
         folium.Marker(marker_coordinates, popup=popup).add_to(m)
-
+        
         # Speichere die Karte in einer HTML-Datei
         m.save("map.html")
 
@@ -52,11 +46,6 @@ if selection == "Karte":
 
         # Zeige die HTML-Karte in der Streamlit-Anwendung
         st.components.v1.html(map_html, height=500)
-
-
-       
-     
-
 
 elif selection == "Analyse":
     st.title("In Arbeit")
