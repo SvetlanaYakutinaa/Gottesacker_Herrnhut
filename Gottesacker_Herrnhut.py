ec5a25e32e50628070a0ba9938152df6e942bc9b
Svetlana Yakutina <132339674+SvetlanaYakutinaa@users.noreply.github.com>
2024-06-10 13:09:04 +0200

    Update Gottesacker_Herrnhut.py

--- Gottesacker_Herrnhut.py
+++ Gottesacker_Herrnhut.py
@@ -3,6 +3,7 @@ import folium
 from streamlit.components.v1 import html
 import pandas as pd
 import streamlit_pandas as sp
+import json
 
 st.set_page_config(page_title="Gottesacker Herrnhut", layout="wide")
 
@@ -30,6 +31,27 @@ if selection == "Karte":
 
         # Zeige die HTML-Karte in der Streamlit-Anwendung
         st.components.v1.html(map_html, height=500)
+        
+        json_data = load_json_data("Daten.json")
+        
+        for item in json_data:
+            folium.Marker(
+                location=[item["latitude"], item["longitude"]],
+                popup=f"""
+                <strong>Name:</strong> {item['name']}<br>
+                <strong>Datum:</strong> {item['Datum']}<br>
+                <strong>Stein:</strong> {item['Stein']}<br>
+                <strong>Reihe:</strong> {item['Reihe']}<br>
+                <strong>Feld:</strong> {item['Feld']}<br>
+                <strong>Index der Gemein-Nachrichten (ab 1765):</strong> {item['Index der Gemein-Nachrichten (ab 1765)']}<br>
+                <strong>URL Index:</strong> <a href="{item['URL_Index']}" target="_blank">Link</a><br>
+                <strong>Digitalisat:</strong> <a href="{item['Digitalisat']}" target="_blank">Link</a><br>
+                <strong>Bild:</strong> <img src="{item['bild']}" width="100"><br>
+                <strong>Urheberrecht:</strong> {item['Uhrheberecht']}
+                """,
+                tooltip=item["name"]
+                
+            ).add_to(m)
 
 
 elif selection == "Analyse":
