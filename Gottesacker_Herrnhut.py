3630659f9dc16a4525d3ad81315a7872c454d2bc
Svetlana Yakutina <132339674+SvetlanaYakutinaa@users.noreply.github.com>
2024-06-18 10:36:57 +0200

    Update Gottesacker_Herrnhut.py

--- Gottesacker_Herrnhut.py
+++ Gottesacker_Herrnhut.py
@@ -91,15 +91,16 @@ if selection == "Karte":
         
      elif option == "Maria Magdalena Bezold":
                st.subheader("Maria Magdalena Bezold")
-               st.image("Gottesacker_Julius_Titz/S3R411.jpg", width=350, use_column_width="always", caption= "© Julius Titzt")
-               st.link_button("Digitalisat", "http://digital.slub-dresden.de/id1766125816/180")
-               folium.Marker([51.019082, 14.748807], popup="Maria Magdalena Bezold. Stein: 11, Reihe: R4, Feld: S3", icon=folium.Icon(color='red')).add_to(m)
                with open("XML/02445.xml", "rb") as file:
                        btn = st.download_button(
                                label="Download XML",
                                data= file,
                                file_name="02445.xml"
                        )
+               st.link_button("Digitalisat", "http://digital.slub-dresden.de/id1766125816/180")
+               st.image("Gottesacker_Julius_Titz/S3R411.jpg", width=350, use_column_width="always", caption= "© Julius Titzt")
+               folium.Marker([51.019082, 14.748807], popup="Maria Magdalena Bezold. Stein: 11, Reihe: R4, Feld: S3", icon=folium.Icon(color='red')).add_to(m)
+               
         
      elif option == "Maria Magdalena Richter":
                st.subheader("Maria Magdalena Richter")
