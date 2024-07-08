34c8dee3d51d21824a2ce6446c3ad4ec4be1806e
Svetlana Yakutina <132339674+SvetlanaYakutinaa@users.noreply.github.com>
2024-07-08 14:41:31 +0200

    Update Gottesacker_Herrnhut.py

--- Gottesacker_Herrnhut.py
+++ Gottesacker_Herrnhut.py
@@ -278,7 +278,7 @@ if selection == "Digitaler Gottesacker":
 
 ##############################
      with st.container():
-        st.write("Literatur")
+        st.header("Literatur")
         st.write("Fischer, Norbert (1996): Vom Gottesacker zum Krematorium: eine Sozialgeschichte der Friedhöfe in Deutschland seit dem 18. Jahrhundert. Diss. Staats-und Universi-tätsbibliothek Hamburg Carl von Ossietzky.")
         st.write("Hennig, Paul Otto (1922): Der Hutberg. Führer über den Gottesacker der Brüder-gemeine : der Gemeine Herrnhut zu ihrem 200 jährigen Bestehen gewidmet. Herrnhut.")
         st.write("N.A. (1822): Der Gottes-Acker zu Herrnhut. Bei der einhundertjährigen Jubel-Feier des am 17ten Juny 1722 begonnenen Anbaues böhmisch-mährischer Brüder der Evangelischen Brüdergemeine, gewidmet von einigen hierzu vereinigten Freunden Hirschberg 1822, Unitätsarchiv Herrnhut, Signatur R 121315 / 36-1.")
@@ -352,7 +352,7 @@ elif selection == "Analyse":
     st.image("Output/POS.png", caption= "Abbild 6: Relative Häufigkeit der verschiedenen Wortarten (Männer vs. Frauen)")
 
     with st.container():
-            st.write("Literatur")
+            st.header("Literatur")
             st.write("Böß, Stephanie (2016): Gottesacker-Geschichten als Gedächtnis. Eine Ethnographie zur Herrnhuter Erinnerungskultur am Beispiel von Neudietendorfer Lebensläufen. Münster/ New York: Waxmann.")
 
 
